9c6258e568918601883a69fee35357e16829fc50
Logan Brooks <lbrooks81@southhills.edu>
2025-03-31 15:20:48 -0400

    Basic Framework
    Added basic framework for the home page

---
+++ Weather App/Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Logan on 3/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
