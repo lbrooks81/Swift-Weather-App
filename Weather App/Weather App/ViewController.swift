141aff0d9a7da5d8305f1fdbd00fa675fa2cd944
Logan Brooks <lbrooks81@southhills.edu>
2025-04-09 14:54:57 -0400

    Container Views are functioning
    Added ContainerViews to the second page

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -7,17 +7,55 @@
 
 import UIKit
 import SwiftUI
+import CoreLocation
 
+
+struct Response: Codable {
+  
+    
+}
 
 class ViewController: UIViewController {
     
-    
-    
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    let apiKey: String = "bfbf4f27dd5879cb9ed45a2afb2f7b8b"
+    let apiEndpoint: String = "https://api.openweathermap.org/data/3.0/onecall/?lat=\(latitude)&lon=\(longitude)"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if let labelVC = segue.destination as? DayView{
+            switch segue.identifier {
+            case "One":
+                break
+            case "Two":
+                break
+            case "Three":
+                break
+            case "Four":
+                break
+            case "Five":
+                break
+            case "Six":
+                break
+            case "Seven":
+                break
+            default:
+                break
+            }
+        }
+    }
+}
+
+class DayView: UIViewController {
+    @IBOutlet var lowHighTempLabel: UILabel!
+    @IBOutlet var weatherDescriptionLabel: UILabel!
+    @IBOutlet var chanceOfPrecipitationLabel: UILabel!
+    @IBOutlet var currentTempLabel: UILabel!
 }
 
 
